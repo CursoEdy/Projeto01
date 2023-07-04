005ea9e243f9ac7e782b1749f65d35cd065b842c
Edy <30.edyalves@gmail.com>
2023-07-03 21:11:00 -0300

    Criando entidade Experiencia

--- src/main/java/com/edyalves/projeto01/Projeto01Application.java
+++ src/main/java/com/edyalves/projeto01/Projeto01Application.java
@@ -1,13 +1,19 @@
 package com.edyalves.projeto01;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Projeto01Application {
+public class Projeto01Application implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(Projeto01Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
 	}
 
 }
---
+++ src/main/java/com/edyalves/projeto01/domain/Experiencia.java
@@ -0,0 +1,101 @@
+package com.edyalves.projeto01.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+import java.util.Objects;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Experiencia implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	private String nome;
+	private String cargo;
+	private String descricao;
+	private Date inicio;
+	private Date termino;
+	
+	public Experiencia() {}
+
+	public Experiencia(Integer id, String nome, String cargo, String descricao, Date inicio, Date termino) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.cargo = cargo;
+		this.descricao = descricao;
+		this.inicio = inicio;
+		this.termino = termino;
+	}
+
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getCargo() {
+		return cargo;
+	}
+
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public Date getInicio() {
+		return inicio;
+	}
+
+	public void setInicio(Date inicio) {
+		this.inicio = inicio;
+	}
+
+	public Date getTermino() {
+		return termino;
+	}
+
+	public void setTermino(Date termino) {
+		this.termino = termino;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Experiencia other = (Experiencia) obj;
+		return Objects.equals(id, other.id);
+	}
+}
